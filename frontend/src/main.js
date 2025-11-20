59d744341106c417c3ee8074a9018147fe6f62d4
masunoob <tomoshima0313@gmail.com>
2025-11-21 00:22:07 +0900

    ElementPlusのアイコンを追加しました。

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import 'element-plus/dist/index.css'
 
 import App from './App.vue'
@@ -13,5 +14,9 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(ElementPlus)
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 
 app.mount('#app')
